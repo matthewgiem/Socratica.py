8afbea1603670ef1057980aa196cc0ebd5c850c7
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-02-07 12:04:40 +0700

    string length

---
+++ if_then.py
@@ -0,0 +1,7 @@
+# Collect string / test length
+
+input = input('Please enter a test string: ')
+
+if len(input) < 6:
+    print('Your string is too short.')
+    print('Please enter a string with at least 6 characters.')
