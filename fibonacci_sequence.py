f2b25ef97a47deffa4eb9c233f9981fc6edf35d7
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-02-18 15:49:15 +0700

    fibonacci sequence recursive function

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,4 +1,6 @@
-def fibonacci(number):
+from functools import lru_cache
+
+def matts_fibonacci(number):
     """returns the nth element in the fibonacci sequence"""
     if type(number) == int:
         if number > 0:
@@ -14,3 +16,42 @@ def fibonacci(number):
             print('choose a number greater than 0')
     else:
         print('choose a whole number')
+
+def fibonacci_slow(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci_slow(n-1) + fibonacci_slow(n-2)
+
+fibonacci_cash = {}
+def cash_fibonacci(n):
+    # If we have cached the value, then return it
+    if n in fibonacci_cash:
+        return fibonacci_cash[n]
+
+    # Compute the Nth term
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 1
+    elif n > 2:
+        value = cash_fibonacci(n - 1) + cash_fibonacci(n - 2)
+
+    # Cache the value and return it
+    fibonacci_cash[n] = value
+    return value
+
+@lru_cache(maxsize = 1000)
+def fibonacci_lru_cache(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci_lru_cache(n-1) + fibonacci_lru_cache(n-2)
+
+# test
+for n in range(1,1001):
+    print(n, ":", fibonacci_lru_cache(n))
