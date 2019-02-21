abfe6c37a3006c5e23e166a9d0fe3c4fb4191af2
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-02-21 12:48:43 +0700

    two more version of a prime number generator

--- prime.py
+++ prime.py
@@ -1,3 +1,6 @@
+import time
+import math
+
 def is_prime_v1(n):
     """Return 'True' if 'n' is a prime number. False otherwise."""
     if n == 1:
@@ -8,6 +11,49 @@ def is_prime_v1(n):
     return True
 
 
+def is_prime_v2(n):
+    """Return 'True' if 'n' is a prime number. False otherwise."""
+    if n == 1:
+        return False
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(2, 1 + max_divisor):
+        if n % d == 0:
+            return False
+    return True
+
+def is_prime_v3(n):
+    """Return 'True' if 'n' is a prime number. False otherwise."""
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    if n > 2 and n % 2 == 0:
+        return False
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(3, 1 + max_divisor, 2):
+        if n % d == 0:
+            return False
+    return True
+
 # === Test Function ===
-for n in range(1, 21):
-    print(n, is_prime_v1(n))
+t0 = time.time()
+for n in range(1, 100000):
+    is_prime_v1(n)
+t1 = time.time()
+print("Time required V1: ", t1 - t0)
+
+t0 = time.time()
+for n in range(1, 100000):
+    is_prime_v2(n)
+t1 = time.time()
+print("Time required V2: ", t1 - t0)
+
+t0 = time.time()
+for n in range(1, 100000):
+    is_prime_v3(n)
+t1 = time.time()
+print("Time required V3: ", t1 - t0)
+
+# 
+# for n in range(1,21):
+#     print(is_prime_v1(n)==is_prime_v3(n))
