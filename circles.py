1bb3a44bd60a80ca17784fb6e26b1b76aa144391
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-02-22 11:32:22 +0700

    create new test

--- circles.py
+++ circles.py
@@ -1,4 +1,6 @@
 from math import pi
 
 def circle_area(r):
+    if r < 0:
+        raise ValueError("The radius cannot be negative.")
     return pi*(r**2)
--- test_circles.py
+++ test_circles.py
@@ -9,3 +9,7 @@ class TestCircleArea(unittest.TestCase):
         self.assertAlmostEqual(circle_area(1), pi)
         self.assertAlmostEqual(circle_area(0), 0)
         self.assertAlmostEqual(circle_area(2.1), pi*2.1**2)
+
+    def test_values(self):
+        # Make sure value errors are raised when necessary
+        self.assertRaises(ValueError, circle_area, -2)
