99649630827627c64cbff545705e455893afe092
Matthew Giem <matthew.giem.programmer@gmail.com>
2019-02-22 13:36:00 +0700

    doc on urllib

---
+++ urllib.py
@@ -0,0 +1,10 @@
+# https:en.wikipedia.org/wiki/URL?key=value&life=42#History
+#
+# Protocol: http, https, ftp, ...
+# Host: en.wikipedia.org
+# Port: http=80, https=443
+# Path: wiki/URL
+# Querystring: key=value&life=42
+# Fragment: History
+
+from urllib import 
